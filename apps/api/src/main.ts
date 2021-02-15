ff9ef8c109c9791a2f35b1123df3a1dfa4df4e28
Brandon <robertsbt@gmail.com>
2021-02-15 15:15:59 -0600

    fix: enable CORS for API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
