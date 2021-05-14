5f608531eadbc54e3b71d00bbed6dc5cde96b91b
Brandon <robertsbt@gmail.com>
2021-05-14 18:53:37 -0500

    chore: cleanup

--- libs/actions/src/lib/twitch.actions.ts
+++ libs/actions/src/lib/twitch.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { Command, Chat, Raid, Sub } from '@ngtwitch/models';
+import { Chat, Command } from '@ngtwitch/models';
 
 export const command = createAction(
   '[Twitch] Chat Command',
@@ -14,19 +14,4 @@ export const broadcast = createAction(
 export const message = createAction(
   '[Twitch] Chat Message',
   props<{ message: Chat }>()
-);
-
-export const raid = createAction(
-  '[Twitch] Raided',
-  props<{ raid: Raid }>()
-);
-
-export const sub = createAction(
-  '[Twitch] Subscription',
-  props<{ sub: Sub }>()
-);
-
-export const follow = createAction(
-  '[Twitch] Follow',
-  props<{ follower: string }>()
 );
